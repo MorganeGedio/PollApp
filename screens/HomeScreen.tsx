f97b6aa9a1c302b7e76870de426a19122f4da321
Morgane Gedio <morgane.gedio@gmail.com>
2020-05-11 11:43:09 +0200

    add TypeScript to the app

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,31 @@
+import * as WebBrowser from 'expo-web-browser';
+import * as React from 'react';
+import { View, Text, Image, StyleSheet } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View
+      style={{
+        flexDirection: "column",
+        height: 100,
+        padding: 20
+      }}
+    >
+      <View style={{ backgroundColor: "blue", flex: 1}} />
+      <Image 
+        style={styles.logo}
+        source={{
+          uri: 'https://images.unsplash.com/photo-1543332164-6e82f355badc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+        }} />
+      <Text>Monday 11 May </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  logo: {
+    width: 200,
+    height: 200,
+  },
+});
+
