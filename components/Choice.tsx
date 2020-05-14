5411f24a1380177661119d3be16aff2826b929f0
Morgane Gedio <morgane.gedio@gmail.com>
2020-05-14 15:28:18 +0200

    implement post request for the vote

--- components/Choice.tsx
+++ components/Choice.tsx
@@ -1,11 +1,14 @@
-import * as React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 export default function Choice(props) {
+  const [visible, setVisible] = useState(false)
+
   return (
-    <TouchableOpacity style={styles.choice}>
-      <View>
+    <TouchableOpacity style={styles.choice} onPress={props.voteChange}>
+      <View style={styles.option}>
         <Text>{props.title}</Text>
+        <Text> - {props.votes}</Text>
       </View>
     </TouchableOpacity>
   );
@@ -16,7 +19,10 @@ const styles = StyleSheet.create({
     backgroundColor: "#ffe8df",
     marginTop: 10,
     padding: 10,
-    justifyContent: "center",
     alignItems: "center",
+  },
+  option: {
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
 });
--- screens/DetailScreen.tsx
+++ screens/DetailScreen.tsx
@@ -7,26 +7,38 @@ import {
   FlatList,
   SafeAreaView,
   Button,
-  TouchableOpacity,
+  Alert,
 } from "react-native";
 import { useRoute } from "@react-navigation/native";
 import { useNavigation } from "@react-navigation/native";
 import Choice from "../components/Choice";
 import apiary from "../apiary";
+import Axios from "axios";
 
 export default function DetailScreen() {
-  const [details, setDetails] = useState({ question: "", choices: [] });
   const route = useRoute();
   const navigation = useNavigation();
 
+  const [details, setDetails] = useState({ question: "", choices: [] });
+
+  const fetchData = async () => {
+    console.log("test")
+    const response = await apiary.get(route.params.url);
+      console.log(response);
+    setDetails(response.data);
+  };
+
   useEffect(() => {
-    const fetchData = async () => {
-      const response = await apiary.get(route.params.url);
-      //   console.log(response);
-      setDetails(response.data);
-    };
     fetchData();
   }, []);
+
+  // vote
+  const chooseOption = async (url) => {
+    // Alert.alert("Test");
+    const info = await apiary.post(url);
+    console.log(info)
+    fetchData();
+  }
 
   return (
     <SafeAreaView>
@@ -38,8 +50,14 @@ export default function DetailScreen() {
         keyExtractor={(item) => item.url}
         data={details.choices}
         renderItem={({ item }) => {
-          //   console.log("item", item);
-          return <Choice title={item.choice}></Choice>;
+            // console.log("item", item);
+          return (
+            <Choice
+              voteChange={() => chooseOption(item.url)}
+              title={item.choice}
+              votes={item.votes}
+            ></Choice>
+          );
         }}
       />
       <Button title="Go back" onPress={() => navigation.goBack()} />
