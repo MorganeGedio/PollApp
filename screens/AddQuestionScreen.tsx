21082cd3d2c456d81b95958d959c4b2543cbeaa2
Morgane Gedio <morgane.gedio@gmail.com>
2020-05-18 17:38:23 +0200

    style the addQuestionScreen

--- screens/AddQuestionScreen.tsx
+++ screens/AddQuestionScreen.tsx
@@ -1,4 +1,10 @@
-import { Text, View, TextInput, Button } from "react-native";
+import {
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
 import React, { useState } from "react";
 import apiary from "../apiary";
 
@@ -21,16 +27,70 @@ export default function AddQuestionScreen() {
   };
 
   return (
-    <View>
-      <Text>New question</Text>
-      <Text>Add the question: </Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Add a new question to the list</Text>
+      <Text style={styles.label}>Write your question: </Text>
       <TextInput
+        style={styles.input}
         onChangeText={(question) => onChangeQuestion(question)}
       ></TextInput>
-      <Text>List the options (separated by a comma): </Text>
-      <TextInput onChangeText={(choice) => onChangeChoice(choice)}></TextInput>
-      <Button title="Submit" onPress={handleSubmit}></Button>
+      <Text style={styles.label}>
+        List the options {"\n"} (separated by a comma):{" "}
+      </Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={(choice) => onChangeChoice(choice)}
+      ></TextInput>
+      <TouchableOpacity style={styles.submit} onPress={handleSubmit}>
+        <Text style={styles.submitText}>SEND ! </Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+  title: {
+    fontFamily: "nunito-bold",
+    textAlign: "center",
+    fontSize: 25,
+    padding: 10,
+  },
+  label: {
+    fontFamily: "nunito-bold",
+    padding: 10,
+    fontSize: 20,
+    textAlign: "center",
+  },
+  input: {
+    height: 50,
+    borderColor: "gray",
+    borderWidth: 0.5,
+    borderRadius: 10,
+    fontFamily: "roboto",
+    fontSize: 20,
+    padding: 10,
+  },
+  submit: {
+    backgroundColor: "#E0F2DB",
+    padding: 10,
+    height: 70,
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 30,
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.15,
+    shadowRadius: 2.62,
+  },
+  submitText: {
+    fontFamily: "nunito-bold",
+    fontSize: 20,
+  },
+});
