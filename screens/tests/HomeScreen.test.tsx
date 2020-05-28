1c727c7e7a6db4cddc2cac488041d3dce1f52ca3
Morgane Gedio <morgane.gedio@gmail.com>
2020-05-28 17:24:13 +0200

    extract FormatDate function & test it

--- screens/tests/HomeScreen.test.tsx
+++ screens/tests/HomeScreen.test.tsx
@@ -4,15 +4,4 @@ import renderer from "react-test-renderer";
 import HomeScreen from "../HomeScreen";
 
 describe("<HomeScreen />", () => {
-
-  it("tests the date function", () => {
-    const wrapper = renderer.create(<HomeScreen />);
-    const inst = wrapper.getInstance();
-    expect(inst.formatDate("2020-05-19")).toMatchSnapshot();
-
-    // HomeScreen.formatDate = jest.fn();
-    // const value = HomeScreen.formatDate("2020-05-19");
-    // expect(value).toBe("19/05/2020");
-  });
 });
-// });
